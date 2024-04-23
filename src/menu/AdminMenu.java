6f919e1d5ada8f8f1c81df3efade2eb0e2547d09
Trung1234 <trungsieunhan24@gmail.com>
2024-04-23 22:06:19 +0700

    add room in menu

--- src/menu/AdminMenu.java
+++ src/menu/AdminMenu.java
@@ -8,6 +8,8 @@ import api.HotelResource;
 import model.Customer;
 import model.IRoom;
 import model.Reservation;
+import model.Room;
+import model.RoomType;
 
 public class AdminMenu {
 	public static void createAdminMenu() {
@@ -47,8 +49,31 @@ public class AdminMenu {
 	}
 
 	public static void addARoom() {
-		// TODO Auto-generated method stub
-		
+		Scanner scanner = new Scanner(System.in);
+        int choice;
+        System.out.println("Enter 1 or 2 to make a choice of Room type.");
+        while (true) {
+            System.out.print("Enter your choice (1 or 2): 1 is SINGLE, 2 is DOUBLE");
+            if (scanner.hasNextInt()) { // Check if input is an integer
+                choice = scanner.nextInt();
+                if (choice == 1 || choice == 2) { // Check if input is 1 or 2
+                    break;
+                } else {
+                    System.out.println("Invalid choice. Please enter 1 or 2.");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter an integer.");
+                scanner.next(); // Consume invalid input
+            }
+        }
+        RoomType roomType = RoomType.getValueByLabel(choice);
+        System.out.println("Please input your roomNumber: ");
+	    String roomNumber = scanner.nextLine();
+	    System.out.println("Please input your roomNumber's price: ");
+	    Double price = scanner.nextDouble();
+        scanner.close();
+        IRoom room = new Room(roomNumber,  price, roomType);
+		AdminResource.addRoom(room);
 	}
 
 	public static void seeAllCustomers() {
--- src/model/RoomType.java
+++ src/model/RoomType.java
@@ -1,18 +1,18 @@
 package model;
 
 public enum RoomType {
-    SINGLE("1"),
-    DOUBLE("2");
+    SINGLE(1),
+    DOUBLE(2);
 
-    public final String label;
+    public final Integer label;
 
-    private RoomType(String label) {
+    private RoomType(Integer label) {
         this.label = label;
     }
 
-    public static RoomType valueOfLabel(String label) {
+    public static RoomType getValueByLabel(Integer label) {
         for (RoomType roomType : values()) {
-            if (roomType.label.equals(label)) {
+            if (label == roomType.label) {
                 return roomType;
             }
         }
